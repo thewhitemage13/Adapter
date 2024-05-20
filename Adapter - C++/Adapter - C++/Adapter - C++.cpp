0fd12d252b3a23f6744577772892e1ee38a3b085
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-20 20:01:52 +0200

    Add files via upload

---
+++ Adapter - C++/Adapter - C++/Adapter - C++.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+using namespace std;
+
+class Scales {
+public:
+	virtual float* GetWeight() = 0;
+	virtual ~Scales(){}
+};
+
+class UkrainScales : public Scales {
+private:
+	float* current_weight;
+public:
+	UkrainScales(float* cw) : current_weight(cw){}
+
+	float* GetWeight() override {
+		return current_weight;
+	}
+
+	~UkrainScales() override {
+		delete current_weight;
+	}
+};
+
+class BritishScale : public Scales {
+private:
+	float* current_weight;
+public:
+	BritishScale(float* cw) : current_weight(cw) {}
+
+	float* GetWeight() override {
+		return current_weight;
+	}
+
+	~BritishScale() override {
+		delete current_weight;
+	}
+};
+
+class AdapterForBritishScales : public Scales {
+private:
+	BritishScale* britishscales;
+	float* cw;
+public:
+	AdapterForBritishScales(BritishScale* bs) : britishscales(bs){}
+	~AdapterForBritishScales() {
+		delete britishscales;
+		delete cw;
+	}
+
+	float* GetWeight() {
+		cw = britishscales->GetWeight();
+		*cw = *cw * 0.453;
+		return cw;
+	}
+};
+
+int main() {
+	float kg = 55.0;
+	float lb = 55.0;
+
+	Scales* UkrainSCales = new UkrainScales(&kg);
+	Scales* BritishSCales = new AdapterForBritishScales(new BritishScale(&lb));
+
+	cout << *UkrainSCales->GetWeight() << "\n";
+	cout << *BritishSCales->GetWeight() << "\n";
+}
